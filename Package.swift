11b4db135aeca87cb6c4878588475656868f592f
Ihor Vasylkov <ihor.vasylkov@start.io>
2023-01-12 16:13:02 +0200

    Updated StartApp SDK package

--- Package.swift
+++ Package.swift
@@ -7,7 +7,6 @@ let package = Package(
     name: "StartioAdmobMediation",
     platforms: [.iOS(.v11)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "StartioAdmobMediation",
             targets: ["StartioAdmobMediation"])
@@ -31,10 +30,10 @@ let package = Package(
 
 extension Package.Dependency {
     static let admob: Package.Dependency = .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: Version("9.13.0"))
-    static let startApp: Package.Dependency = .package(url: "https://gitlab.hosts-app.com/sdk/ios-sdk-swift-package.git", branch: "master")
+    static let startApp: Package.Dependency = .package(url: "https://github.com/StartApp-SDK/StartAppSDK-SwiftPackage.git", from: Version("4.9.0"))
 }
 
 extension Target.Dependency {
     static let GoogleMobileAds: Target.Dependency = .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads")
-    static let StartApp: Target.Dependency = .product(name: "StartApp", package: "ios-sdk-swift-package")
+    static let StartApp: Target.Dependency = .product(name: "StartApp", package: "StartAppSDK-SwiftPackage")
 }
