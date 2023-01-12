f1593aa0e8d8adeb137dbe75f60c93f1c675e4ba
ihor-startio <96785617+ihor-startio@users.noreply.github.com>
2023-01-12 17:24:38 +0200

    Adopt spm (#3)
    * Updated adapter to support AdMob 9.13. Updated StartAppSDK to 4.8.0. Raised adapter version to 1.2.0.
    
    * Changes after code review
    
    * Added swift package support
    
    * Refactored package by adding explicit dependency constants
    
    * updated StartApp dependency to latest version
    
    * Updated StartApp SDK package
    
    Co-authored-by: Start.io <pavlo.kurliandskyi@start.io>
    Co-authored-by: Pavlo Kurlyandskiy <pavlo.kurliandskyi@startapp.com>

---
+++ Package.swift
@@ -0,0 +1,39 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "StartioAdmobMediation",
+    platforms: [.iOS(.v11)],
+    products: [
+        .library(
+            name: "StartioAdmobMediation",
+            targets: ["StartioAdmobMediation"])
+    ],
+    dependencies: [
+        .admob,
+        .startApp
+    ],
+    targets: [
+        .target(
+            name: "StartioAdmobMediation",
+            dependencies: [
+                .StartApp,
+                .GoogleMobileAds
+            ],
+            path: "StartioAdmobMediation",
+            publicHeadersPath: ""
+        )
+    ]
+)
+
+extension Package.Dependency {
+    static let admob: Package.Dependency = .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: Version("9.13.0"))
+    static let startApp: Package.Dependency = .package(url: "https://github.com/StartApp-SDK/StartAppSDK-SwiftPackage.git", from: Version("4.9.0"))
+}
+
+extension Target.Dependency {
+    static let GoogleMobileAds: Target.Dependency = .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads")
+    static let StartApp: Target.Dependency = .product(name: "StartApp", package: "StartAppSDK-SwiftPackage")
+}
